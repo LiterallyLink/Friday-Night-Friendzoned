5a5a0c5b67910e46d3dad6def1de6446803080fe
Meow <67402756+LiterallyLink@users.noreply.github.com>
2024-10-27 14:54:18 -0700

    Updated Build Bat

--- art/build_x64.bat
+++ art/build_x64.bat
@@ -6,5 +6,8 @@ haxelib run lime build windows -release
 echo.
 echo done.
 pause
-pwd
-explorer.exe export\release\windows\bin
+
+cd export\release\windows\bin
+echo Attempting to run PsychEngine.exe...
+start "" "%CD%\PsychEngine.exe"
+pause
